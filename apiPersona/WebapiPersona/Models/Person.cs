ea2046a44e03d47e1334e5d594ecc3b54de2e53c
Sergio Daniel Quevedo <a2019114381@estudiantes.upsa.edu.bo>
2021-01-18 21:52:56 -0400

    Modelo persona creado

---
+++ apiPersona/WebapiPersona/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace WebapiPersona.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public enum SexoType
+    {
+        Femenino=0,
+        Masculino=1
+    }
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+        [Required]
+        [StringLength(80,ErrorMessage ="Favor ingrese su nombre complete entre 5 y 80 caracteres",MinimumLength =5)]
+        public string Name { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode =true,DataFormatString ="{0:dd/MM/yyyy}")]
+        public DateTime Birthday { get; set; }
+
+        public SexoType Sex { get; set; } 
+    }
+}
